5c0a4ed801e3d4ac741efd1b34c69b221f3931f4
Justin Wang <droitinjuste@gmail.com>
2025-01-10 17:59:20 -0500

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/sections/hero";
+
 export default function Home() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <>
+    <Hero />
+    </>
+  );
 }
---
+++ src/sections/hero.tsx
@@ -0,0 +1,14 @@
+export default function hero() {
+    return (
+      <section className="hero">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              title
+            </h1>
+          </div>
+        </div>
+      </section>
+    );
+}
+  
